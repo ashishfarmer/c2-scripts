26eda51332fe60d4e1cbda7736a83d1a672d3eed
Ashish <ashish.farmer@amd.com>
2018-03-29 13:43:26 -0700

    Made running inference on single image more usable through command line

--- scripts/run_inference_single_img.py
+++ scripts/run_inference_single_img.py
@@ -7,71 +7,11 @@ import numpy as np
 import skimage.io
 import skimage.transform
 from matplotlib import pyplot
-import os, time
+import os, time, getopt, sys
 from caffe2.python import core, workspace, models
 import urllib2
 import operator
 import caffe2.python._import_c_extension as C
-print("Required modules imported.")
-
-# Configuration --- Change to your setup and preferences!
-# This directory should contain the models downloaded from the model zoo. To run this 
-#   tutorial, make sure there is a 'squeezenet' directory at this location that 
-#   contains both the 'init_net.pb' and 'predict_net.pb'
-CAFFE_MODELS = "/home/ashish/work/caffe2-integration/models/caffe2"
-
-# Some sample images you can try, or use any URL to a regular image.
-# IMAGE_LOCATION = "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Whole-Lemon.jpg/1235px-Whole-Lemon.jpg"
-# IMAGE_LOCATION = "https://upload.wikimedia.org/wikipedia/commons/7/7b/Orange-Whole-%26-Split.jpg"
-# IMAGE_LOCATION = "https://upload.wikimedia.org/wikipedia/commons/a/ac/Pretzel.jpg"
-# IMAGE_LOCATION = "https://cdn.pixabay.com/photo/2015/02/10/21/28/flower-631765_1280.jpg"
-IMAGE_LOCATION = "/home/ashish/work/data/ILSVRC2012_img_val/ILSVRC2012_val_00000011.JPEG"
-
-# What model are we using?
-#    Format below is the model's: <folder, INIT_NET, predict_net, mean, input image size>
-#    You can switch 'squeezenet' out with 'bvlc_alexnet', 'bvlc_googlenet' or others that you have downloaded
-MODEL = 'bvlc_alexnet', 'init_net.pb', 'predict_net.pb', 'ilsvrc_2012_mean.npy', 227
-
-# codes - these help decypher the output and source from a list from ImageNet's object codes 
-#    to provide an result like "tabby cat" or "lemon" depending on what's in the picture 
-#   you submit to the CNN.
-codes =  "https://gist.githubusercontent.com/aaronmarkham/cd3a6b6ac071eca6f7b4a6e40e6038aa/raw/9edb4038a37da6b5a44c3b5bc52e448ff09bfe5b/alexnet_codes"
-print("Config set!")
-
-# set paths and variables from model choice and prep image
-CAFFE_MODELS = os.path.expanduser(CAFFE_MODELS)
-
-# mean can be 128 or custom based on the model
-# gives better results to remove the colors found in all of the training images
-MEAN_FILE = os.path.join(CAFFE_MODELS, MODEL[0], MODEL[3])
-"""
-if not os.path.exists(MEAN_FILE):
-    print("No mean file found!")
-    mean = 128
-else:
-    print ("Mean file found!")
-    mean = np.load(MEAN_FILE).mean(1).mean(1)
-    mean = mean[:, np.newaxis, np.newaxis]
-"""
-print("No mean file found!")
-mean = 128
-print("mean was set to: ", mean)
-
-# some models were trained with different image sizes, this helps you calibrate your image
-INPUT_IMAGE_SIZE = MODEL[4]
-
-# make sure all of the files are around...
-INIT_NET = os.path.join(CAFFE_MODELS, MODEL[0], MODEL[1])
-PREDICT_NET = os.path.join(CAFFE_MODELS, MODEL[0], MODEL[2])
-
-# Check to see if the files exist
-if not os.path.exists(INIT_NET):
-    print("WARNING: " + INIT_NET + " not found!")
-else:
-    if not os.path.exists(PREDICT_NET):
-        print("WARNING: " + PREDICT_NET + " not found!")
-    else:
-        print("All needed files found!")
 
 # Function to crop the center cropX x cropY pixels from the input image
 def crop_center(img,cropx,cropy):
@@ -99,29 +39,86 @@ def rescale(img, input_height, input_width):
         imgScaled = skimage.transform.resize(img, (input_width, input_height))
     return imgScaled
 
+
+
+opts, args = getopt.getopt(sys.argv[1:], 'm:i:s:g:')
+
+model = ''
+inp_img = ''
+img_size = 0
+use_gpu = 0
+
+for opt, arg in opts:
+    if opt == '-m':
+        model = arg
+    elif opt == '-i':
+        inp_img = arg
+    elif opt == '-s':
+        img_size = int(arg)
+    elif opt == '-g':
+        use_gpu = int(arg)
+
+if model == '' or inp_img == '' or img_size == 0:
+    print('Invalid command line argument')
+    print('Usage:')
+    print('-m <model directory> -i <input image> -s <input image size> -g [Use GPU option -- 0: CPU (default), 1: HIP, 2: MIOPEN]')
+    print('Model directory must contain both the init_net.pb and predict_net.pb, and optionally ilsvrc_2012_mean.npy files')
+    exit()
+
+# codes - these help decypher the output and source from a list from ImageNet's object codes 
+#    to provide an result like "tabby cat" or "lemon" depending on what's in the picture 
+#   you submit to the CNN.
+codes =  "https://gist.githubusercontent.com/aaronmarkham/cd3a6b6ac071eca6f7b4a6e40e6038aa/raw/9edb4038a37da6b5a44c3b5bc52e448ff09bfe5b/alexnet_codes"
+
+# set paths and variables from model choice and prep image
+model = os.path.expanduser(model)
+
+# mean can be 128 or custom based on the model
+# gives better results to remove the colors found in all of the training images
+mean_file = os.path.join(model, 'ilsvrc_2012_mean.npy')
+
+if not os.path.exists(mean_file):
+    print("No mean file found!")
+    mean = 128
+else:
+    print ("Mean file found!")
+    mean = np.load(mean_file).mean(1).mean(1)
+    mean = mean[:, np.newaxis, np.newaxis]
+
+# make sure all of the files are around...
+INIT_NET = os.path.join(model, 'init_net.pb')
+PREDICT_NET = os.path.join(model, 'predict_net.pb')
+
+# Check to see if the files exist
+if not os.path.exists(INIT_NET):
+    print("WARNING: " + INIT_NET + " not found!")
+else:
+    if not os.path.exists(PREDICT_NET):
+        print("WARNING: " + PREDICT_NET + " not found!")
+
 # Load the image as a 32-bit float
 #    Note: skimage.io.imread returns a HWC ordered RGB image of some size
-img = skimage.img_as_float(skimage.io.imread(IMAGE_LOCATION)).astype(np.float32)
-print("Original Image Shape: " , img.shape)
+img = skimage.img_as_float(skimage.io.imread(inp_img)).astype(np.float32)
+#print("Original Image Shape: " , img.shape)
 
 # Rescale the image to comply with our desired input size. This will not make the image 227x227
 #    but it will make either the height or width 227 so we can get the ideal center crop.
-img = rescale(img, INPUT_IMAGE_SIZE, INPUT_IMAGE_SIZE)
-print("Image Shape after rescaling: " , img.shape)
+img = rescale(img, img_size, img_size)
+#print("Image Shape after rescaling: " , img.shape)
 pyplot.figure()
 pyplot.imshow(img)
 pyplot.title('Rescaled image')
 
 # Crop the center 227x227 pixels of the image so we can feed it to our model
-img = crop_center(img, INPUT_IMAGE_SIZE, INPUT_IMAGE_SIZE)
-print("Image Shape after cropping: " , img.shape)
+img = crop_center(img, img_size, img_size)
+#print("Image Shape after cropping: " , img.shape)
 pyplot.figure()
 pyplot.imshow(img)
 pyplot.title('Center Cropped')
 
 # switch to CHW (HWC --> CHW)
 img = img.swapaxes(1, 2).swapaxes(0, 1)
-print("CHW Image Shape: " , img.shape)
+#print("CHW Image Shape: " , img.shape)
 
 #pyplot.figure()
 for i in range(3):
@@ -141,15 +138,22 @@ img = img * 255 - mean
 # add batch size axis which completes the formation of the NCHW shaped input that we want
 img = img[np.newaxis, :, :, :].astype(np.float32)
 
-print("NCHW image (ready to be used as input): ", img.shape)
+#print("NCHW image (ready to be used as input): ", img.shape)
 pyplot.show()
 
 # Create GPU device option
 device_opts = caffe2_pb2.DeviceOption()
-device_opts.device_type = caffe2_pb2.HIP
-device_opts.hip_gpu_id = 0
-engine_list = ['MIOPEN', '']
-C.set_global_engine_pref({caffe2_pb2.HIP : engine_list})
+if use_gpu == 0:
+    device_opts.device_type = caffe2_pb2.CPU
+    print('Running on CPU')
+else:
+    device_opts.device_type = caffe2_pb2.HIP
+    device_opts.hip_gpu_id = 0
+    print('Running on HIP')
+    if use_gpu == 2:
+        engine_list = ['MIOPEN', '']
+        C.set_global_engine_pref({caffe2_pb2.HIP : engine_list})
+        print('Using MIOPEN')
 
 C.feed_blob('data', img, device_opts.SerializeToString())
 
@@ -165,13 +169,24 @@ with open(PREDICT_NET, 'rb') as f:
     net_def.device_option.CopyFrom(device_opts)
     C.create_net(net_def.SerializeToString())
 
+C.feed_blob('data', img, device_opts.SerializeToString())
+### Debug code
+"""
+for b in workspace.Blobs():
+    x = workspace.FetchBlob(b)
+    if type(x) != str:
+        print(str(b) + ': ' + str(x.shape))
+
+"""
+### End of debug code
+
 print('Running net ' + workspace.GetNetName(net_def) + '...')
 
 C.run_net(workspace.GetNetName(net_def), 1, False)
 
 # Turn it into something we can play with and examine which is in a multi-dimensional array
 results = workspace.FetchBlob('prob')
-print("results shape: ", results.shape)
+#print("results shape: ", results.shape)
 
 # Quick way to get the top-1 prediction result
 # Squeeze out the unnecessary axis. This returns a 1-D array of length 1000
